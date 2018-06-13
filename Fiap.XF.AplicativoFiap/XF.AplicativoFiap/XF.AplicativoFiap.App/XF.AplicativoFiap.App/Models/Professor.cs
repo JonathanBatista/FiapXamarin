1cf1b81f7e90f3c9a5c5569096da5c1d757593d4
jbsilva <jonathan.batistasilva@outlook.com>
2018-06-13 20:06:29 -0300

    primeiro commit atividade 4

---
+++ Fiap.XF.AplicativoFiap/XF.AplicativoFiap/XF.AplicativoFiap.App/XF.AplicativoFiap.App/Models/Professor.cs
@@ -0,0 +1,11 @@
+﻿namespace XF.AplicativoFiap.App.Models
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Titulo { get; set; }
+    }
+}
