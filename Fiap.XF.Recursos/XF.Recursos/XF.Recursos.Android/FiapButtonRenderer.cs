824f81e2e0c17d951fc6b1209f958ed4466bae0c
jonathan <jonathan.batistasilva@outlook.com>
2018-06-16 11:50:37 -0300

    add new features

---
+++ Fiap.XF.Recursos/XF.Recursos/XF.Recursos.Android/FiapButtonRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XF.Recursos.CustomControl;
+using XF.Recursos.Droid;
+
+[assembly: ExportRenderer(typeof(FiapButton), typeof(FiapButtonRenderer))]
+namespace XF.Recursos.Droid
+{
+    class FiapButtonRenderer : ButtonRenderer
+    {
+        public FiapButtonRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Gray);
+            }
+        }
+    }
+}
